c1ec5cae9c7c9e0d6a01394a452d2f4adc0dc545
Shane Park <psh40963@naver.com>
2021-11-02 09:16:18 +0900

    h2 태그 렌더링 수정 및 이미지 모달 가운데 정렬

--- projects/java/src/main/java/com/tistory/shanepark/string/fileName.java
+++ projects/java/src/main/java/com/tistory/shanepark/string/fileName.java
@@ -3,13 +3,26 @@ package com.tistory.shanepark.string;
 public class fileName {
 
     public static void main(String[] args) {
-        String destinationFileNameOriginal = "admin@전화번호패턴.txt";
+        String netId = netIdProcessor("abc@abcd.net");
+        String destinationFileNameOriginal = netId + "@전화번호패턴.txt";
         String suffix = "(2)";
         StringBuffer destinationFileName = new StringBuffer(destinationFileNameOriginal);
-        int dotIndex = destinationFileName.indexOf(".");
+        int dotIndex = destinationFileName.lastIndexOf(".");
         destinationFileName.insert(dotIndex, suffix);
 
         System.out.println(destinationFileName);
+
+        String fileName = "admin@전화번호패턴.txt";
+        System.out.println(getFileName(fileName));
+    }
+
+    private static String netIdProcessor(String netId) {
+        int indexAt = netId.indexOf("@");
+        return indexAt < 0 ? netId : netId.substring(0, indexAt);
+    }
+
+    private static String getFileName(String str) {
+        return str.substring(str.indexOf("@")+1);
     }
 
 }
