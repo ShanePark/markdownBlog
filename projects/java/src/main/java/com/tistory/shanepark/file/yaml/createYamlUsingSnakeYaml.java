885280e4140c91d2a568a8bff1b8a80c4f09106f
Shane Park <psh40963@naver.com>
2021-11-17 00:02:20 +0900

    converted Map to LinkedHashMap for sequence

--- projects/java/src/main/java/com/tistory/shanepark/file/yaml/createYamlUsingSnakeYaml.java
+++ projects/java/src/main/java/com/tistory/shanepark/file/yaml/createYamlUsingSnakeYaml.java
@@ -4,24 +4,26 @@ import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class createYamlUsingSnakeYaml {
     final static String folder = System.getProperty("user.home") + "/Documents";
 
     public static void main(String[] args) throws IOException {
-        Map<String, Object> data1 = new HashMap<String, Object>();
+        Map<String, Object> data1 = new LinkedHashMap<>();
         data1.put("name", "shane");
         data1.put("nationality", "korean");
         data1.put("jobs", new String[]{"programmer", "traveler"});
+        data1.put("timestamp", new Date());
 
-        Map<String, Object> data2 = new HashMap<String, Object>();
+        Map<String, Object> data2 = new LinkedHashMap<String, Object>();
         data2.put("name", "shane2");
         data2.put("nationality", "english");
         data2.put("jobs", new String[]{"programmer"});
 
-        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> result = new LinkedHashMap<>();
         result.put("data1", data1);
         result.put("data2", data2);
 
