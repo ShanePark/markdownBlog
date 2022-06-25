b8f3d11b14b75d83a085fb18aaf8a5238d2ec8cd
shane <psh40963@naver.com>
2022-06-25 23:07:49 +0900

    Update CharArray_VS_CharAt.java
    changed text and made the array size bit bigger

--- projects/java/src/main/java/com/tistory/shanepark/string/CharArray_VS_CharAt.java
+++ projects/java/src/main/java/com/tistory/shanepark/string/CharArray_VS_CharAt.java
@@ -4,7 +4,7 @@ public class CharArray_VS_CharAt {
 
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < 200_000_000; i++) {
+        for (int i = 0; i < 300_000_000; i++) {
             sb.append('a');
         }
 
@@ -15,7 +15,7 @@ public class CharArray_VS_CharAt {
 
         start = System.currentTimeMillis();
         char[] chars = str.toCharArray();
-        System.out.println("array생성 소요시간: " + (System.currentTimeMillis() - start) + "ms");
+        System.out.println("array 생성 소요시간: " + (System.currentTimeMillis() - start) + "ms");
 
         start = System.currentTimeMillis();
         for (int i = 0; i < chars.length; i++) {
@@ -27,7 +27,7 @@ public class CharArray_VS_CharAt {
         for (int i = 0; i < str.length(); i++) {
             c = str.charAt(i);
         }
-        System.out.println("charAt 소요시간: " + (System.currentTimeMillis() - start) + "ms");
+        System.out.println("charAt 순회 소요시간: " + (System.currentTimeMillis() - start) + "ms");
 
     }
 
